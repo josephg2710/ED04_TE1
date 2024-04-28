5aab226160d1373663bc5427e9ec837767852ddd
josephg2710 <27josephgelrud@gmail.com>
2024-04-28 13:41:57 -0600

    Punto 4

--- src/main/java/com/mycompany/ud4_te1/Main.java
+++ src/main/java/com/mycompany/ud4_te1/Main.java
@@ -4,16 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         CCuenta cuentaTrabajo = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
-        operativaCuenta(cuentaTrabajo);
+        double cantidadARetirar = 2300; // Define aquí la cantidad a retirar
+        operativaCuenta(cuentaTrabajo, cantidadARetirar);
     }
 
-    public static void operativaCuenta(CCuenta cuenta) {
+    public static void operativaCuenta(CCuenta cuenta, double cantidad) {
         double saldoActual = cuenta.estado();
         System.out.println("El saldo actual es " + saldoActual);
 
         try {
             System.out.println("Cargo en cuenta");
-            cuenta.retirar(2300);
+            cuenta.retirar(cantidad);
             System.out.println("El nuevo saldo es: " + cuenta.estado());
         } catch (Exception e) {
             System.out.println("Fallo al retirar: " + e.getMessage());
@@ -21,7 +22,7 @@ public class Main {
         
         try {
             System.out.println("Abono en cuenta");
-            cuenta.ingresar(695);
+            cuenta.ingresar(695); // La cantidad a ingresar sigue siendo fija en este ejemplo
             System.out.println("El nuevo saldo es: " + cuenta.estado());
         } catch (Exception e) {
             System.out.println("Fallo al ingresar: " + e.getMessage());
